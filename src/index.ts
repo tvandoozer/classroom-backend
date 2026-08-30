d01f4f890c41835c5ee2b16ef6b2214a3678e2f6
Travis <travis@vandoozer.com>
2026-08-30 07:56:10 -0600

    implement changes

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ const PORT = 8000;
 
 app.use(express.json());
 
+if (!process.env.FRONTEND_URL) throw new Error("FRONTEND_URL is not set in .env file");
+
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
